d023eb4ec01bf9747246c1b6a59870be0e5f6926
wangluosepi <63545293+wangluosepi@users.noreply.github.com>
2024-02-19 20:34:19 +0800

    Update index.js

--- TimeTableT/pages/index/index.js
+++ TimeTableT/pages/index/index.js
@@ -1,10 +1,19 @@
 // index.js
 // 获取应用实例
-const app = getApp()
-
 Page({
   data: {
-  
+    //myName:[],
+    //college:[]
+    getData:[]
+  },
+  onLoad: function (options) {
+    let that = this
+    that.setData({
+      //myName:options.myData.nickName,
+      //college:options.college
+      getData:JSON.parse(options.myData)
+    })
+    console.log(that.data.getData)
   },
   // 事件处理函数
   onClick:function() {
